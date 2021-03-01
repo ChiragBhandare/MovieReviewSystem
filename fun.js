c00304a39b51e88233523db5f8e2c77b79e3f165
ChiragBhandare <79807379+ChiragBhandare@users.noreply.github.com>
2021-03-01 19:56:16 +0530

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,58 @@
+
+    function print() {
+        document.getElementById("span").innerHTML = document.getElementById("uname").value;
+        document.getElementById("span1").innerHTML = document.getElementById("rating").value;
+    }
+
+    function print1() {
+        document.getElementById("span2").innerHTML = document.getElementById("uname1").value;
+        document.getElementById("span3").innerHTML = document.getElementById("rating1").value;
+    }
+
+    function print2() {
+        document.getElementById("span4").innerHTML = document.getElementById("uname2").value;
+        document.getElementById("span5").innerHTML = document.getElementById("rating2").value;
+    }
+
+    function print3() {
+        document.getElementById("span6").innerHTML = document.getElementById("uname3").value;
+        document.getElementById("span7").innerHTML = document.getElementById("rating3").value;
+    }
+
+
+    function print4() {
+        document.getElementById("span8").innerHTML = document.getElementById("uname4").value;
+        document.getElementById("span9").innerHTML = document.getElementById("rating4").value;
+    }
+
+    function print5() {
+        document.getElementById("span10").innerHTML = document.getElementById("uname5").value;
+        document.getElementById("span11").innerHTML = document.getElementById("rating5").value;
+    }
+
+
+    function print6() {
+        document.getElementById("span12").innerHTML = document.getElementById("uname6").value;
+        document.getElementById("span13").innerHTML = document.getElementById("rating6").value;
+    }
+
+
+    function print7() {
+        document.getElementById("span14").innerHTML = document.getElementById("uname7").value;
+        document.getElementById("span15").innerHTML = document.getElementById("rating7").value;
+    }
+
+
+    function print8() {
+        document.getElementById("span16").innerHTML = document.getElementById("uname8").value;
+        document.getElementById("span17").innerHTML = document.getElementById("rating8").value;
+    }
+
+
+    function print9() {
+        document.getElementById("span18").innerHTML = document.getElementById("uname9").value;
+        document.getElementById("span19").innerHTML = document.getElementById("rating9").value;
+    }
+
+    
+
